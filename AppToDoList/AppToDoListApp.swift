a41bc15aae72cf020870cc721b55fa1d98ffb7cf
leoalvesv <leoalvesvianna@gmail.com>
2025-08-26 15:13:15 -0300

    Initial Commit

---
+++ AppToDoList/AppToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppToDoListApp.swift
+//  AppToDoList
+//
+//  Created by Leonardo Alves Viana on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AppToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
